db8b73d59bdebf48b6ecc4bcb5bfd554f66ab9b3
RitaJeveaux <ritajeveaux@gmail.com>
2025-09-19 08:37:06 -0300

    removerTarefa concluido

--- src/ListaDeTarefas.ts
+++ src/ListaDeTarefas.ts
@@ -30,16 +30,22 @@ export class ListaDeTarefas {
 
   }
 
+
   private encontrarTarefaPorId(id: number): Tarefa | undefined {
     return this._tarefas.find(tarefa => tarefa.id === id);
   }
 
 
-  public removerTarefa(id: number): void {
-    const tarefa = this.encontrarTarefaPorId(id);
-    if (tarefa) {
+  private encontrarIndicePorId(id: number): number {
+    return this._tarefas.findIndex(tarefa => tarefa.id === id);
+  }
 
-      console.log(`Tarefa ${tarefa.descricao} removida.`);
+
+  public removerTarefa(id: number): void {
+    const indice = this.encontrarIndicePorId(id);
+    if (indice != -1) {
+      const tarefaRemovida = this._tarefas.splice(indice,1)[0]!;
+      console.log(`Tarefa ${tarefaRemovida.descricao} removida.`);
     } else {
       console.log(`Tarefa com ID: ${id}, não encontrada.`)
     }
