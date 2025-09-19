f2c2040750df17d65f51655dd61d44ff0c37a21f
RitaJeveaux <ritajeveaux@gmail.com>
2025-09-19 09:22:02 -0300

    testando remover tarefa

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,10 @@ minhaLista.marcarTarefaComoConcluida(2);
 minhaLista.listarTarefas();
 
 minhaLista.marcarTarefaComoConcluida(5);
+
+console.log('//////////////////////////');
+console.log('Testando Exercicio Aula 01');
+console.log('//////////////////////////');
+
+minhaLista.removerTarefa(1);
+minhaLista.listarTarefas();
